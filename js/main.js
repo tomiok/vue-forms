e3d0d79ad6bd87ac3d6bc1dd263b638dfc6b9ea5
Tomas Lingotti <tomaslingotti@gmail.com>
2022-01-29 15:04:14 -0300

    form and computed properties in vue

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const app = Vue.createApp({
     data: () => ({
-        title: "Contact form"
-    })
+        title: "Contact form",
+        user: {
+            name: "",
+            age: 0,
+            languages: [],
+            genre: "m"
+        }
+    }),
+    methods: {
+        handleSubmit() {
+            console.log(this.user)
+        }
+    },
+    computed: {
+        completeName() {
+            return this.user.name + " " + this.user.age
+        }
+    }
 });
 
 const mountApp = app.mount("#app")
